d9d4c1bc2653d3e715001120e2fffef7d891b04f
Kasturi0405 <kasturibala.a@gmail.com>
2026-09-02 17:02:03 +0530

    Add files via upload
    Comitted day 1 class assigment

---
+++ javaScriptBasics1.js
@@ -0,0 +1,13 @@
+var firstName = 'Kasturi'
+var companyName ='XYZ'
+var mobileNumber = 7654670978
+var isAutomation = true
+var hasPlaywright
+
+console.log(firstName +" "+typeof firstName);
+console.log(companyName +" "+typeof companyName);
+console.log(mobileNumber +" "+typeof mobileNumber);
+console.log(isAutomation +" "+typeof isAutomation);
+console.log(hasPlaywright +" "+typeof hasPlaywright);
+//shift+alt+f -> formatting
+//shift+alt+a -. paragraph comments
